45f6824b14a686a2683341cebfda1d8bc99a4066
Jordao Ezequiel - jordaotalk <Jordao Ezequiel - jordaotalk@gmail.com>
2010-11-29 04:30:01 -0300

    modificacoes no menu de opcoes

--- Opcoes.py
+++ Opcoes.py
@@ -24,25 +24,68 @@ class Opcoes:
                         pygame.display.set_mode((608,672), FULLSCREEN, 32)
                     elif event.key == K_DOWN:
                         pygame.display.set_mode((608,672), 0, 32)
-                        
-            Objetos.background.blit(Objetos.fundoOpcoes, (0,0))
+
+            print mouse_pos
+            
             this.texto = Objetos.fonteHoliday.render("OPCOES",True, (255,255,255))
 
-            modoTela = Objetos.fonteHoliday.render("MODO DE TELA", True, (0,0,0))
-            fullscreen = Objetos.fonteHoliday.render("Tela Cheia", True, (0,0,0))
-            modoNormal = Objetos.fonteHoliday.render("Tela Normal", True, (0,0,0))
+            modoTela = Objetos.fonteHoliday.render("MODO DE TELA", True, (255,255,255))
+            fullscreen = Objetos.fonteHoliday.render("Tela Cheia", True, (255,255,255))
+            modoNormal = Objetos.fonteHoliday.render("Tela Normal", True, (255,255,255))
 
-            idioma = Objetos.fonteHoliday.render("IDIOMA", True, (0,0,0))
-            portugues = Objetos.fonteHoliday.render("Portugues", True, (0,0,0))
-            ingles = Objetos.fonteHoliday.render("Ingles", True, (0,0,0))
-            espanhol = Objetos.fonteHoliday.render("Espanhol", True, (0,0,0))
+            idioma = Objetos.fonteHoliday.render("IDIOMA", True, (255,255,255))
+            portugues = Objetos.fonteHoliday.render("Portugues", True, (255,255,255))
+            ingles = Objetos.fonteHoliday.render("Ingles", True, (255,255,255))
+            espanhol = Objetos.fonteHoliday.render("Espanhol", True, (255,255,255))
 
-            volume = Objetos.fonteHoliday.render("VOLUME", True, (0,0,0))
+            volume = Objetos.fonteHoliday.render("VOLUME", True, (255,255,255))
+            volumeNum = Objetos.fonteHoliday.render("00", True, (255,255,255))
+
             
+            if 100 <= mouse_pos[0] <= 280 and 185 <= mouse_pos[1] <= 210:
+                fullscreen = Objetos.fonteHoliday.render("Tela Cheia", True, (255,0,0))
+                if mouse_press[0]:
+                    pygame.display.set_mode((608,672), FULLSCREEN, 32)
+
+            elif 320 <= mouse_pos[0] <= 525 and 185 <= mouse_pos[1] <= 210:
+                modoNormal = Objetos.fonteHoliday.render("Tela Normal", True, (255,0,0))
+                if mouse_press[0]:
+                    pygame.display.set_mode((608,672), 0, 32)
+
+            elif 70 <= mouse_pos[0] <= 235 and 335 <= mouse_pos[1] <= 362:
+                portugues = Objetos.fonteHoliday.render("Portugues", True, (255,0,0))
+                if mouse_press[0]:
+                    pass
+            elif 270 <= mouse_pos[0] <= 370 and 335 <= mouse_pos[1] <= 362:
+                ingles = Objetos.fonteHoliday.render("Ingles", True, (255,0,0))
+                if mouse_press[0]:
+                    pass
+            elif 410 <= mouse_pos[0] <= 558 and 335 <= mouse_pos[1] <= 362:
+                espanhol = Objetos.fonteHoliday.render("Espanhol", True, (255,0,0))
+                if mouse_press[0]:
+                    pass
+
             
 
+
+
+            Objetos.background.blit(Objetos.fundoOpcoes, (0,0))
 
             Objetos.background.blit(this.texto, (240, 30))
+
+            Objetos.background.blit(modoTela,(170, 130))
+            Objetos.background.blit(fullscreen,(100, 180))
+            Objetos.background.blit(modoNormal,(320, 180))
+            
+            Objetos.background.blit(idioma,(240, 280))
+            Objetos.background.blit(portugues,(70,330))
+            Objetos.background.blit(ingles,(270,330))
+            Objetos.background.blit(espanhol,(410,330))
+
+            Objetos.background.blit(volume,(240,430))
+            Objetos.background.blit(volumeNum, (280,480))
+            
+            
 
             pygame.display.update()
 
